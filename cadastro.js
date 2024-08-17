4ea051e7f4caf29792f56ff6ac14390a40243a81
thiagolutes <160236286+thiagolutes@users.noreply.github.com>
2024-08-16 21:58:32 -0300

    Add files via upload

--- cadastro.js
+++ cadastro.js
@@ -1,11 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('eye-closed').style.display = 'block';
-    document.getElementById('eye-open').style.display = 'none';
+    onInit();
 });
 
 document.getElementById('fotoPreview').style.display = 'none';
 document.getElementById('fotoDefault').style.display = 'block';
 let fotoBase64 = '';
+
+function onInit(){
+    document.getElementById('fotoPreview').style.display = 'none';
+    document.getElementById('fotoDefault').style.display = 'flex';
+    verificarLogin();
+    if (localStorage.getItem('isLogado') === 'true') {
+        const nomeLogado = localStorage.getItem('nome');
+        const base64Fto = localStorage.getItem('profilePic');
+
+        if(base64Fto){
+            const ftoPerfil = document.getElementById('ftoDePerfil');    
+            ftoPerfil.src = base64Fto
+        }
+        document.getElementById('nomeLogado').innerText = nomeLogado
+    }
+}
 
 function openFileDialog() {
     const input = document.createElement('input');
@@ -33,47 +48,15 @@ function handleFileSelect(event) {
             fotoPreview.style.display = 'block';
             fotoDefault.style.display = 'none';
             const imageDataUrl = reader.result;
-            profilePic.src = imageDataUrl;
             localStorage.setItem('profilePic', imageDataUrl);
         };
         
         reader.readAsDataURL(file);
     } else {
-
         fotoPreview.style.display = 'none';
         fotoDefault.style.display = 'block';
     }
 }
-
-
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-
-    var nome = document.getElementById('nome').value;
-    var fotoInput = document.getElementById('foto');
-
-    function convertImageToBase64(file, callback) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            callback(e.target.result);
-        };
-        reader.readAsDataURL(file);
-    }
-
-    if (fotoInput.files.length > 0) {
-        var fotoFile = fotoInput.files[0];
-        
-        convertImageToBase64(fotoFile, function(base64Image) {
-            localStorage.setItem('isLogado', true);
-            localStorage.setItem('nome', nome);
-            localStorage.setItem('fotoPerfil', base64Image);
-
-            alert('Cadastro realizado com sucesso!');
-        });
-    } else {
-        alert('Por favor, selecione uma foto.');
-    }
-});
 
 function mostrarMsg(message, type) {
     var messageElement = document.getElementById('message');
@@ -102,18 +85,16 @@ function togglePassword(inputId, eyeClosedId, eyeOpenId) {
     }
 }
 
- function verificarLogin() {
+function verificarLogin() {
     const isLogado = localStorage.getItem('isLogado') === 'true';
     document.querySelector('.cadastro').style.display = isLogado ? 'none' : 'block';
     document.querySelector('.cadastrado').style.display = isLogado ? 'block' : 'none';
 }
 
-
-document.addEventListener('DOMContentLoaded', verificarLogin);
-
 function deslogar() {
     localStorage.removeItem('nome');
     localStorage.removeItem('isLogado');
+    localStorage.removeItem('profilePic');
     mostrarMsg('Sucesso ao deslogar!', 'sucesso');
     verificarLogin();
 }
@@ -132,6 +113,8 @@ function cadastrar() {
         return;    
     } else if (!senha1) {
         mostrarMsg('Digite sua senha', 'erro');
+        return;        
+    } else if (!verificarSenha(senha1)) {
         return;
     } else if (senha1 !== senha2) {
         mostrarMsg('As senhas não são iguais!', 'erro');
@@ -141,6 +124,14 @@ function cadastrar() {
         localStorage.setItem('isLogado', 'true');
         localStorage.setItem('nome', nome);
         verificarLogin();
+        onInit();
     }
+}
 
+function verificarSenha(senha1) {
+    if (senha1.length <= 3) {
+        mostrarMsg('A senha deve conter mais de 3 caracteres', 'erro');
+        return false; 
+    }
+    return true;
 }
